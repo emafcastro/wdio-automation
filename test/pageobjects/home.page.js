b9fe8b2224d061961bc0a8fba9198784186cd11c
Emanuel Castro <castrofemanuel@gmail.com>
2022-11-25 15:19:46 -0300

    Add product to cart test, new script to generate report added in package.json

--- test/pageobjects/home.page.js
+++ test/pageobjects/home.page.js
@@ -15,8 +15,44 @@ class HomePage extends Page {
         return $("//header//span[@class='logged-in'][contains(text(),'Welcome')]");
     }
 
+    get productList() {
+        return $$(".product-item-info");
+    }
+
+    get sizeProduct(){
+        // This will get only the first size option in the first product
+        return $("//li[@class='product-item'][1]//div[contains(@aria-describedby,'option-label-size')][1]");
+    }
+
+    get colorProduct(){
+        // This will get only the first color in the first product
+        return $("//li[@class='product-item'][1]//div[contains(@aria-describedby,'option-label-color')][1]");
+    }
+
+    get addToCartButton(){
+        // This will get only the button in the first product
+        return $("//li[@class='product-item'][1]//button");
+    }
+
+    get productAddedToCartMessage(){
+        return $("//div[@role='alert']/div/div");
+    }
+
+
     open(){
         return super.open();
+    }
+
+    async addProductToCart(){
+        await this.sizeProduct.click();
+        await this.colorProduct.click();
+        await this.productList[0].moveTo();
+        await this.addToCartButton.click();
+    }
+
+    async getProductAddedToCartMessage(){
+        await this.productAddedToCartMessage.waitForDisplayed({timeout: 5000});
+        return this.productAddedToCartMessage.getText();
     }
 }
 
